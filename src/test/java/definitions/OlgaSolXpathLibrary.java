16a972f9684fccea463a2027ed954ddb4c052d2a
olgasuperstars <olgasuperstar@mail.ru>
2023-09-25 20:19:51 +0600

    Тестовое задание по вакансии "Специалист по проверке домашни заданий"

--- src/test/java/definitions/OlgaSolXpathLibrary.java
+++
@@ -1,7 +0,0 @@
-package definitions;
-
-public class OlgaSolXpathLibrary {
-    public static String oMedicenter1URL = "https://medicenter-qa1.vercel.app/";
-    public static String oMedicenter2URL = "https://medicenter-qa2.vercel.app/";
-    public static String oLoginButton = "//button[contains(text(),'Login')]";
-}
